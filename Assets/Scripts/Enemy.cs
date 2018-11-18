557791c17247e995f5fa1604e513fef523dd901b
Anics Chacko <43536206+anicschacko@users.noreply.github.com>
2018-11-19 01:28:00 +0530

    Intial Commit
    First Commit to the Project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private readonly float radius = 3f;
+    private float angle;
+    private bool isSkipped = false;
+
+    public GameObject asteroidsPrefab, asteroidsHold;
+
+    // Use this for initialization
+    void Start()
+    {
+        angle = 0;
+        InvokeRepeating("SpawnBorder", 0.01f, 2f);
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        asteroidsHold.transform.Translate(Vector3.back * 20 * Time.deltaTime);
+    }
+
+
+    void SpawnBorder()
+    {
+        while(angle <= 360)
+        {
+            if (Random.Range(0, 5) != 0)
+            {
+                Vector3 pos = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * radius, Mathf.Sin(angle * Mathf.Deg2Rad) * radius, 120f);
+                GameObject ep = Instantiate(asteroidsPrefab, pos, Quaternion.identity);
+                ep.transform.SetParent(asteroidsHold.transform);
+            }
+            else
+                angle += 19;
+            angle += 19;
+        }
+        angle = 0; 
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private float radius = 3f;
+
+    private float angle;
+	
+	void Start ()
+    {
+        angle = 270;	
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            angle -= 2f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            angle += 2f;
+        }
+        //angle = Mathf.Clamp(angle, 180, 360);
+
+        transform.position = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * radius, Mathf.Sin(angle * Mathf.Deg2Rad) * radius, transform.position.z);
+
+        //transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+	}
+}
